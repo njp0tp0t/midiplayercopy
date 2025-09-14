3bf97437c95f06acf9bb5cdfbbe12a7566e8d093
Noel Joseph Jovero <njjovero26@gmail.com>
2025-09-13 23:57:42 -0400

    couldnt finish question 58

---
+++ __SHELL12.java
@@ -0,0 +1,12 @@
+
+public class __SHELL12 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\njjov\\Downloads\\projects\\bluej-projects\\Chapter03\\midi-player");
+final TuneCreator tuneCrea1 = (TuneCreator)__bluej_runtime_scope.get("tuneCrea1");
+
+final SimpleTune simpleTu1 = (SimpleTune)__bluej_runtime_scope.get("simpleTu1");
+
+
+tuneCrea1.CreateAndPlay();
+
+}}
